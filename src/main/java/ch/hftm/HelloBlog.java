7307d0d00cca91c07cf758edcda3ece351caad8d
Davor Andrijasevic <d.andrijasevic95@gmail.com>
2025-05-09 15:25:15 +0200

    Welcome to Blog Message added

--- src/main/java/ch/hftm/HelloBlog.java
+++ src/main/java/ch/hftm/HelloBlog.java
@@ -5,13 +5,13 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
-public class GreetingResource {
+@Path("/blog")
+public class HelloBlog {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
+    public String blog(){
+        return "Willkommen auf meinem Blog!";
     }
 
 }
